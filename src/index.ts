c556f2da21a1e01fbb2527858a3da152ef5e062e
nquocnghia <nquocnghia@gmail.com>
2017-07-10 21:36:41 +0200

    Draw first animation in canvas
    1. Draw canvas, game and ship
    2. Explain why this is a bad practice in pov of OOP (loose coupling & high cohesion)

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,45 @@ import { Constant } from './constant';
     // get canvas context
     const ctx = canvas.getContext('2d');
 
+    // load ship's image
+    const img = new Image();
+    img.src = 'assets/player_ship.png';
+
+    // init ship's state
+    let x = Constant.GAME_LEFT, y = Constant.GAME_TOP;
+    const width = 64, height = 48;
+    let deltaX = 5, deltaY = 5;
+
     // main loop
     function tick() {
+        // canvas background
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, Constant.CANVAS_WIDTH, Constant.CANVAS_HEIGHT);
+
+        // game background
+        ctx.fillStyle = 'white';
+        ctx.fillRect(
+            Constant.GAME_LEFT,
+            Constant.GAME_TOP,
+            Constant.GAME_WIDTH,
+            Constant.GAME_HEIGHT
+        );
+
+        // draw ship image
+        ctx.drawImage(img, x, y, width, height);
+
+        // adjust ship's position
+        x += deltaX;
+        y += deltaY;
+
+        // adjust ship's movement: make it bounce
+        if (x <= Constant.GAME_LEFT || x + width - 1 >= Constant.GAME_RIGHT) {
+            deltaX *= -1;
+        }
+
+        if (y <= Constant.GAME_TOP || y + height - 1 >= Constant.GAME_BOTTOM) {
+            deltaY *= -1;
+        }
 
     }
 
